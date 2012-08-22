8c0fddb8439231eb6e1949827e27b19684bfc923
Rafael Brigagão Paulino <rafa_corre@macbook-pro-de-rafael.local>
2012-08-22 10:58:08 -0300

    Initial Commit

---
+++ CadastroAlunoTests/CadastroAlunoTests.m
@@ -0,0 +1,32 @@
+//
+//  CadastroAlunoTests.m
+//  CadastroAlunoTests
+//
+//  Created by Rafael Brigagão Paulino on 22/08/12.
+//  Copyright (c) 2012 Rafael Brigagão Paulino. All rights reserved.
+//
+
+#import "CadastroAlunoTests.h"
+
+@implementation CadastroAlunoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CadastroAlunoTests");
+}
+
+@end
